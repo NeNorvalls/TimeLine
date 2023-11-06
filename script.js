da015bba453cbefcd365f10333130056f3a50819
NeNorvalls <honeygeeh27@gmail.com>
2023-11-06 04:46:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const portfolioURL = 'https://nenorvalls-projects-showcase.netlify.app/';
+
+const goBackButton = document.getElementById('goBackButton');
+
+goBackButton.addEventListener('click', function() {
+  window.location.href = portfolioURL;
+});
